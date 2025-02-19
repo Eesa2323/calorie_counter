b2f9bf8e7fe26bb00117369838f045ff9eecf9be
Eesa Munir <23683290@stu.mmu.ac.uk>
2025-02-19 19:12:14 +0000

    Added dependencies, and setup basic project structure for the server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
 // Server code
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors({origin: "*", allowedHeaders: ["x-auth-token"], exposedHeaders: ["x-auth-token"]}))
+
+app.get("/", async (req, res) => {
+    res.send("hey")
+})
+
+const PORT = 6000
+app.listen(6000, () => console.log(`Listening on port: ${PORT}`))
