216fdf3a0294475f5b4274d194d215905ebe8fd7
Eesa Munir <23683290@stu.mmu.ac.uk>
2025-02-21 11:20:54 +0000

    rANDOM

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,5 +2,9 @@
 const express = require('express')
 const router = express.Router()
 
+// ELLO
 router.post("/signup", (req, res) => {
+    const {username, password} = req.body
+    if (!username|| !password) return res.status(400).send('Could not signup')
+
 })
