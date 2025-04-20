ed0e74e72d944c9bd81cde22e573e0c2fb606bf7
LeonelFidalgo <leonelf2406@outlook.com>
2025-04-20 12:34:39 +0100

    Update Setup.js

--- client/Setup.js
+++ client/Setup.js
@@ -46,6 +46,21 @@ function nextStep(step) {
     // If it's the final step (6), redirect to home.html after short delay
   if (step === 6) {
     setTimeout(() => {
+        const currentWeight = document.getElementById('current-weight').value;
+        const targetWeight = document.getElementById('target-weight').value;
+        const height = document.getElementById('height').value;
+        const age = document.getElementById('age').value;
+        const gender = document.querySelector('input[name="gender"]:checked').value;
+
+        const userData = {
+            currentWeight,
+            targetWeight,
+            height,
+            age,
+            gender
+        };
+
+    localStorage.setItem('userSetupData', JSON.stringify(userData));
       window.location.href = "home.html";
     }, 1500); // Adjust delay as needed (e.g. 1.5 seconds)
   }
